54be3cc13350c75b2914065733cc2cbc6f22350b
Raymond Blum <raymond@Raymonds-MacBook-Air.local>
2016-04-10 09:28:14 -0400

    Added some panel mapping functions

--- flipe.ino
+++ flipe.ino
@@ -1,11 +1,37 @@
 #include <SoftwareSerial.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define ROWS 7
+#define COLUMNS 28
+#define PANELS 4
 
+typedef struct _column { bool row[ROWS]; } _column;
+typedef struct _panel { _column column[COLUMNS]; } _panel;
+struct kiosk { _panel panel[PANELS]; } kiosk;
+
+// Set a specified dot on a specified panel
+void setPanelDot(int panelIdx, int x, int y, bool dotOn) {
+  kiosk.panel[panelIdx].column[x].row[y] = dotOn;
+}
+
+// Set all dots on a specified panel
+void setPanel(int panelIdx, bool dotsOn) {
+  for (int x=0; x<COLUMNS; x++) {
+    for (int y=0; y<ROWS; y++) {
+      setPanelDot(panelIdx, x, y, dotsOn);
+    }
+  }
+}
+
+// Set all dots on all panels
+void setPanels(bool dotsOn) {
+  for (int p=0; p<PANELS; p++) {
+    setPanel(p, dotsOn);
+  }
+}
+
+void setup() {
+  setPanels(false);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
 }
