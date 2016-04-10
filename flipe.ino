1aaeeb1c4bacd28aae9cfcb95bd9d2d6959008cf
Raymond Blum <raymond@Raymonds-MacBook-Air.local>
2016-04-10 11:34:07 -0400

    Added some panel addressing and test routines

--- flipe.ino
+++ flipe.ino
@@ -13,6 +13,11 @@ void setPanelDot(int panelIdx, int x, int y, bool dotOn) {
   kiosk.panel[panelIdx].column[x].row[y] = dotOn;
 }
 
+// Get a specified dot on a specified panel
+bool getPanelDot(int panelIdx, int x, int y) {
+  return kiosk.panel[panelIdx].column[x].row[y];
+}
+
 // Set all dots on a specified panel
 void setPanel(int panelIdx, bool dotsOn) {
   for (int x=0; x<COLUMNS; x++) {
@@ -20,6 +25,18 @@ void setPanel(int panelIdx, bool dotsOn) {
       setPanelDot(panelIdx, x, y, dotsOn);
     }
   }
+}
+
+char** emitPanel(int panelIdx) {
+  char **panel = new(char*[28]);
+  for (int x=0; x<COLUMNS; x++) {
+    char *row = new(char[7]);
+    panel[x] = row;
+    for (int y=0; y<ROWS; y++) {
+      row[y]=getPanelDot(panelIdx, x, y)?'X':' ';
+    }
+  }
+  return panel;
 }
 
 // Set all dots on all panels
@@ -31,6 +48,29 @@ void setPanels(bool dotsOn) {
 
 void setup() {
   setPanels(false);
+  setPanelDot(0, 2, 2, true);
+  setPanel(1, true);
+  setPanelDot(1, 2, 2, false);
+  Serial.begin(9600);
+  Serial.println("/setup");
+  dumpPanel(0);
+  dumpPanel(1);
+}
+
+void dumpPanel(int panelIdx) {
+  Serial.print("<");
+  Serial.print(panelIdx);
+  Serial.println(">");
+  char** panel = emitPanel(panelIdx);
+  for (int x=0; x<COLUMNS; x++) {
+    for (int y=0; y<ROWS; y++) {
+      Serial.print(panel[x][y]);
+    }
+    Serial.println();
+  }
+  Serial.print("</");
+  Serial.print(panelIdx);
+  Serial.println(">");
 }
 
 void loop() {
