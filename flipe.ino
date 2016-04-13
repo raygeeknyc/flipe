1bcf5cec2994aa71841deeb63f5ac21cb90483c4
Raymond Blum <raymond@Raymonds-MacBook-Air.local>
2016-04-12 21:37:19 -0400

    Remove the intermediate canvas and work directly from the letterMap. Add idle pattern of random pixels around the letterForm as negative space. Still need to add sensor value scaling and high watermark.

--- flipe.ino
+++ flipe.ino
@@ -7,6 +7,10 @@
 const int ROWS = PANEL_ROWS * PANELS;
 const int COLUMNS = PANEL_COLUMNS;
 
+// between 0 and 99, the "percentage" of attract mode pixels to turn on
+#define ATTRACT_MODE_DENSITY 20
+
+// The pins and BAUD rate to use for the RS485 port
 #define RS485_TX 2
 #define RS485_RX 3
 #define RS485_BAUD 9600
@@ -20,113 +24,69 @@ bool letterMap[COLUMNS][ROWS];
 // Time after the last sensor update before we go to attract mode
 #define IDLE_TIMEOUT 3000
 
-typedef struct _column { bool row[PANEL_ROWS]; } _column;
-typedef struct _panel { _column column[PANEL_COLUMNS]; } _panel;
-struct kiosk { _panel panel[PANELS]; } kiosk;
-
 #define PANEL_HEADER 0x80
 #define PANEL_WRITE_CMD 0x84
 #define PANEL_REFRESH_CMD 0x82
 #define PANEL_END 0x8F
 
-// Set a specified dot in the display
-void setDot(int x, int y, bool dotOn) {
-  int panelIdx = y / PANEL_ROWS;
-  setPanelDot(panelIdx, x , y, dotOn);
-}
-
-// Get a specified dot in the display
-bool getDot(int x, int y) {
-  int panelIdx = y / PANEL_ROWS;
-  return getPanelDot(panelIdx, x , y);
-}
-
-// Set a specified dot on a specified panel
-void setPanelDot(int panelIdx, int x, int y, bool dotOn) {
-  kiosk.panel[panelIdx].column[x].row[y] = dotOn;
-}
-
-// Get a specified dot on a specified panel
-bool getPanelDot(int panelIdx, int x, int y) {
-  return kiosk.panel[panelIdx].column[x].row[y];
-}
-
-// Set all dots on a specified panel
-void setPanel(int panelIdx, bool dotsOn) {
-  for (int x=0; x<PANEL_COLUMNS; x++) {
-    for (int y=0; y<PANEL_ROWS; y++) {
-      setPanelDot(panelIdx, x, y, dotsOn);
-    }
-  }
-}
-
-char** emitPanel(int panelIdx) {
-  char **panel = new(char*[28]);
-  for (int x=0; x<PANEL_COLUMNS; x++) {
-    char *row = new(char[7]);
-    panel[x] = row;
-    for (int y=0; y<PANEL_ROWS; y++) {
-      row[y]=getPanelDot(panelIdx, x, y)?'X':' ';
-    }
-  }
-  return panel;
-}
-
-void refreshPanels() {
+void refreshDisplay() {
   String displayMessage = "";
-  displayMessage+=PANEL_HEADER;
-  displayMessage+=PANEL_REFRESH_CMD;
-  displayMessage+=PANEL_END;
+  displayMessage += PANEL_HEADER;
+  displayMessage += PANEL_REFRESH_CMD;
+  displayMessage += PANEL_END;
   writeToPanels(displayMessage);
 }
 
-void writeDisplayToPanels() {
-  for (int p=0; p<PANELS; p++) {
+byte panelAddress(int panelIdx) {
+  return byte(panelIdx);
+}
+
+bool getDot(bool idle, int x, int y) {
+  bool letterPixel = letterMap[x][y];
+
+  if (idle) {
+    if (letterPixel) {
+      letterPixel != letterPixel;
+    } else {
+      letterPixel = (random(0,100) < ATTRACT_MODE_DENSITY?true:false);
+    }
+  }
+  return letterPixel;
+}
+
+void setDisplay(bool idle, int sensorValue) {
+  // TODO(raymond) Use the sensorValue to scale the rows that are plotted and maintain an aged high watermark for the VU meter effect
+  for (int p = 0; p < PANELS; p++) {
     String displayMessage = "";
-    displayMessage+=PANEL_HEADER;
-    displayMessage+=PANEL_WRITE_CMD;
-    displayMessage+=byte(p);
-    for (int x=0; x<PANEL_COLUMNS; x++) {
+    displayMessage += PANEL_HEADER;
+    displayMessage += PANEL_WRITE_CMD;
+    displayMessage += panelAddress(p);
+    for (int x = 0; x < PANEL_COLUMNS; x++) {
       byte val = 0x00;
-      for (int y=0; y<PANEL_ROWS; y++) {
-        if (getPanelDot(p, x, y)) {
+      for (int y = 0; y < PANEL_ROWS; y++) {
+        if (getDot(idle, x, y)) {
           val |= y;
         }
       }
       displayMessage += val;
     }
-    displayMessage+=PANEL_END;
+    displayMessage += PANEL_END;
     writeToPanels(displayMessage);
   }
 }
 
 void writeToPanels(String message) {
-  panelPort.print(message);    
-}
-
-// Set all dots on all panels
-void setPanels(bool dotsOn) {
-  for (int p=0; p<PANELS; p++) {
-    setPanel(p, dotsOn);
-  }
-}
-
-// Set the dots of a given row in the display to their row in the letter
-// reverse true sets the letter dots off rather than on
-void setDisplayLetterRow(int row, bool reverse) {
-  for (int x=0; x<COLUMNS; x++) {
-    setDot(x, row, reverse?!(letterMap[x][row]):(letterMap[x][row]));
-  }
+  panelPort.print(message);
 }
 
 // Set the letter map
 void _initLetterMap() {
   // Set all dots off
-  for (int x=0; x<COLUMNS; x++) {
-    for (int y=0; y<ROWS; y++) {
+  for (int x = 0; x < COLUMNS; x++) {
+    for (int y = 0; y < ROWS; y++) {
       letterMap[x][y] = false;
-      }
-   }
+    }
+  }
   // Set the dots of an 'e' on
   letterMap[3][11] = true;
   letterMap[3][12] = true;
@@ -453,50 +413,11 @@ void _initLetterMap() {
   letterMap[24][21] = true;
 }
 
-// Set the entire display to a specified dot value
-void _initDisplay(bool dotState) {
-  for (int x=0; x<COLUMNS; x++) {
-    for (int y=0; y<ROWS; y++) {
-      setDot(x, y, dotState);
-    }
-  }
-}
-
 void setup() {
-  _initDisplay(false);
   _initLetterMap();
   panelPort.begin(RS485_BAUD);
   Serial.begin(9600);
   Serial.println("/setup");
-  dumpPanels();
-}
-
-/** Call this once the panels contents are set and you wish to display them
- */
-void updateDisplay() {
-  writeDisplayToPanels();
-  refreshPanels();
-}
-
-void dumpPanels() {
-  for (int p=0; p<PANELS; p++) {
-    dumpPanel(p);
-  }
-}
-void dumpPanel(int panelIdx) {
-  Serial.print("<");
-  Serial.print(panelIdx);
-  Serial.println(">");
-  char** panel = emitPanel(panelIdx);
-  for (int x=0; x<PANEL_COLUMNS; x++) {
-    for (int y=0; y<PANEL_ROWS; y++) {
-      Serial.print(panel[x][y]);
-    }
-    Serial.println();
-  }
-  Serial.print("</");
-  Serial.print(panelIdx);
-  Serial.println(">");
 }
 
 int getSensorValue() {
@@ -505,32 +426,32 @@ int getSensorValue() {
   return 10;
 }
 
-bool setPanelsFromSensor() {
+bool setDisplayForSensor() {
   int sensorReading = getSensorValue();
   if (sensorReading < 0) {
     return false;
   }
-  //TODO(raymond) set the panels here
+  setDisplay(false, sensorReading);
   return true;
 }
 
-void setPanelIdlePattern() {
-  //TODO(raymond) Set some random pixels outside of the "e" to on, set the "e" off
+void setDisplayForIdle() {
+  setDisplay(true, 0);
 }
 
 void loop() {
   unsigned long int nextRefreshAt, lastRefreshed, lastUpdated, idleAt;
-  bool updated = setPanelsFromSensor();
+  bool updated = setDisplayForSensor();
   if (updated) {
     lastUpdated = millis();
     idleAt = lastUpdated + IDLE_TIMEOUT;
   }
   if (millis() > idleAt && idleAt > 0) {
-    setPanelIdlePattern();
+    setDisplayForIdle();
   }
   if (millis() > nextRefreshAt && nextRefreshAt > 0) {
-    updateDisplay();
+    refreshDisplay();
     lastRefreshed = millis();
-    nextRefreshAt = lastRefreshed + REFRESH_FREQUENCY;    
+    nextRefreshAt = lastRefreshed + REFRESH_FREQUENCY;
   }
 }
